37ca5fe3bf58a1b62bca9bdeb93cdd23241da1c4
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-13 09:22:18 -0800

    Add code style changes to socketAPI

--- node-webservice/socketApi.js
+++ node-webservice/socketApi.js
@@ -8,16 +8,28 @@ io.on('connection', function(socket){
     console.log("user connected");
     var currentRoom = "no-room";
 
-    socket.on('message', function(msg){
-        console.log("message");
-      io.to(currentRoom).emit('message', msg);
+    // when user sends a message
+    socket.on('message', function(messageContent){
+        // forward the sent message to everyone in the current room
+        io.to(currentRoom).emit('message', messageContent);
     });
 
+    // when user joins a game, specifying a game ID
     socket.on('joingame', function (gameID) {
+        // leave any room already joined
         socket.leaveAll();
+
+        // join the specified room
         socket.join(gameID);
         currentRoom = gameID;
+
+        // send message back to client saying they have joined this room
         socket.emit("connected-to-gameid", gameID);
+    });
+
+    // when user disconnects
+    socket.on('disconnect', function () {
+       console.log("user disconnected");
     });
 });
 
