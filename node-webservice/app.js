4e754c9045dda51f1e8f176d9a0764099de58c8a
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-12 22:26:16 -0800

    Remove unused routes

--- node-webservice/app.js
+++ node-webservice/app.js
@@ -23,8 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
