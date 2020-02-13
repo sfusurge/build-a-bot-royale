95ecd3c5519d5e394742ab5ca5ca69d211891de6
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-13 15:52:38 -0800

    Add react router

--- node-webservice/react-client/src/App.js
+++ node-webservice/react-client/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import background from './background.jpg';
 import TextInput from './Components/TextInput'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 const backgroundStyle = {
   backgroundImage:`url(${background})`,
@@ -30,17 +37,29 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
-          <h1 style={{textAlign:'center'}}>Build-a-Bot Royale</h1>
-          <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
-        </header>
-        <div className="App-body" style={backgroundStyle}>
-          <TextInput/>
-          <p>API status: { this.state.apiResponse }</p>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
+            <h1 style={{textAlign:'center'}}>Build-a-Bot Royale</h1>
+            <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
+          </header>
+          <div className="App-body" style={backgroundStyle}>
+            <Switch>
+              <Route path="/" exact>
+                <TextInput/>
+                <p>API status: { this.state.apiResponse }</p>
+              </Route>
+              <Route path="/game">
+                <p>Play game here!</p>
+              </Route>
+              <Route>
+                <p>404! No route matched!</p>
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
