c38b897dc35f1c426b53d2e31d2c2befc31ac649
SheaJanke <sheajanke@gmail.com>
2020-02-15 15:01:01 -0800

    The center part of the robot is the parent of all the others.

--- BotRoyaleUnity/Assets/Scripts/buildRobot.cs
+++ BotRoyaleUnity/Assets/Scripts/buildRobot.cs
@@ -7,57 +7,80 @@ using System;
 
 public class buildRobot : MonoBehaviour
 {
-    public GameObject block,center,spike;
-    public Vector3 pos,rot;
-    
+    public GameObject block, center, spike;
 
-// Start is called before the first frame update
-void Start()
-    {   
+
+    // Start is called before the first frame update
+    void Start()
+    {
         string jsonString = File.ReadAllText("Assets/Scripts/test.json");
-        var N = JSON.Parse(jsonString);
-        string test = N[0]["type"];
-        int a = 0;
-        while(N[a] != null){
-            pos = new Vector3((float)N[a]["x"],0.5f,(float)N[a]["y"]);
-            string type = N[a]["type"];
-            Quaternion rot;
-            string direcction = N[a]["direction"];
-            switch(direcction){
-                case "north":
-                    rot = Quaternion.Euler(90,0,0);
-                    break;
-                case "south":
-                    rot = Quaternion.Euler(-90,0,0);
-                    break;
-                case "east":
-                    rot = Quaternion.Euler(0,0,-90);
-                    break;
-                case "west":
-                    rot = Quaternion.Euler(0,0,90);
-                    break;
-                default:
-                    rot = Quaternion.Euler(0,0,0);
-                    break;
+        var json = JSON.Parse(jsonString);
+        int index = 0;
+        GameObject centerParent = null;
+        while (json[index] != null && centerParent == null)
+        {
+            if (json[index]["type"] == "center")
+            {
+                Vector3 pos = new Vector3((float)json[index]["x"], 0.5f, (float)json[index]["y"]);
+                Quaternion rot = Quaternion.Euler(0, 0, 0);
+                centerParent = Instantiate(center, pos, rot);
             }
-            switch(type){
-                case "block":
-                    Instantiate(block, pos, rot);  
-                    break;
-                case "center":
-                    Instantiate(center, pos, rot);
-                    break;
-                case "spike":
-                    Instantiate(spike, pos, rot);
-                    break;
-            }
-            a++;
+            index++;
         }
+        index = 0;
+        while (json[index] != null)
+        {
+            string type = json[index]["type"];
+            if (type != "center")
+            {
+                GameObject childPart = null;
+                Vector3 pos = new Vector3((float)json[index]["x"], 0.5f, (float)json[index]["y"]);
+                Quaternion rot;
+                string direction = json[index]["direction"];
+                switch (direction)
+                {
+                    case "north":
+                        rot = Quaternion.Euler(90, 0, 0);
+                        break;
+                    case "south":
+                        rot = Quaternion.Euler(-90, 0, 0);
+                        break;
+                    case "east":
+                        rot = Quaternion.Euler(0, 0, -90);
+                        break;
+                    case "west":
+                        rot = Quaternion.Euler(0, 0, 90);
+                        break;
+                    default:
+                        rot = Quaternion.Euler(0, 0, 0);
+                        break;
+                }
+                switch (type)
+                {
+                    case "block":
+                        childPart = Instantiate(block, pos, rot);
+                        break;
+                    case "center":
+                        childPart = Instantiate(center, pos, rot);
+                        break;
+                    case "spike":
+                        childPart = Instantiate(spike, pos, rot);
+                        break;
+                }
+                setParent(centerParent,childPart);
+            }
+            index++;
+
+        }
+    }
+
+    void setParent(GameObject parent, GameObject child){
+        child.transform.parent = parent.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
