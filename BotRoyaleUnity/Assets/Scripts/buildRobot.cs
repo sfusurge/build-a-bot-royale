e32b7932baf685b3b1725474c933d3beb38c7bf9
SheaJanke <sheajanke@gmail.com>
2020-02-14 22:50:46 -0800

    JSON rotation works.

--- BotRoyaleUnity/Assets/Scripts/buildRobot.cs
+++ BotRoyaleUnity/Assets/Scripts/buildRobot.cs
@@ -20,8 +20,26 @@ void Start()
         int a = 0;
         while(N[a] != null){
             pos = new Vector3((float)N[a]["x"],0.5f,(float)N[a]["y"]);
-            Quaternion rot = Quaternion.Euler(0, 0, 0);
             string type = N[a]["type"];
+            Quaternion rot;
+            string direcction = N[a]["direction"];
+            switch(direcction){
+                case "north":
+                    rot = Quaternion.Euler(90,0,0);
+                    break;
+                case "south":
+                    rot = Quaternion.Euler(-90,0,0);
+                    break;
+                case "east":
+                    rot = Quaternion.Euler(0,0,-90);
+                    break;
+                case "west":
+                    rot = Quaternion.Euler(0,0,90);
+                    break;
+                default:
+                    rot = Quaternion.Euler(0,0,0);
+                    break;
+            }
             switch(type){
                 case "block":
                     Instantiate(block, pos, rot);  
