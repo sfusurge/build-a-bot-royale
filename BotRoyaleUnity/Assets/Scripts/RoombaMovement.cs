6dee6e27e650b13823742c73a862dc5842fdfb2b
SheaJanke <sheajanke@gmail.com>
2020-02-15 21:00:37 -0800

    Added Exceptions, made parent an empty object and updated roomba movement.

--- BotRoyaleUnity/Assets/Scripts/RoombaMovement.cs
+++ BotRoyaleUnity/Assets/Scripts/RoombaMovement.cs
@@ -22,20 +22,27 @@ public class RoombaMovement : MonoBehaviour
         // never stop this coroutine.
         while (true)
         {
-            float newDirection = Random.Range(-180f,180f);
-            float rotationSpeed = Random.Range(0.5f,1.5f);
+            float newDirection = Random.Range(-180f, 180f);
+            float rotationSpeed = Random.Range(30f, 80f);
             float totalRotation = 0;
-            if(newDirection < 0){
-                while(totalRotation > newDirection){
-                    transform.Rotate(Vector3.forward, -rotationSpeed);
-                    totalRotation -= rotationSpeed;
-                    yield return new WaitForSeconds(0.01f);
+            if (newDirection < 0)
+            {
+                while (totalRotation > newDirection)
+                {
+                    var rotationThisFrame = rotationSpeed * Time.deltaTime;
+                    transform.Rotate(Vector3.forward, -rotationThisFrame);
+                    totalRotation -= rotationThisFrame;
+                    yield return null;
                 }
-            }else{
-                while(totalRotation < newDirection){
-                    transform.Rotate(Vector3.forward, rotationSpeed);
-                    totalRotation += rotationSpeed;
-                    yield return new WaitForSeconds(0.01f);
+            }
+            else
+            {
+                while (totalRotation < newDirection)
+                {
+                    var rotationThisFrame = rotationSpeed * Time.deltaTime;
+                    transform.Rotate(Vector3.forward, rotationThisFrame);
+                    totalRotation += rotationThisFrame;
+                    yield return null;
                 }
             }
         }
--- BotRoyaleUnity/Assets/Scripts/buildRobot.cs
+++ BotRoyaleUnity/Assets/Scripts/buildRobot.cs
@@ -5,7 +5,7 @@ using SimpleJSON;
 using System.IO;
 using System;
 
-public class buildRobot : MonoBehaviour
+public class BuildRobot : MonoBehaviour
 {
     public GameObject block, center, spike;
 
@@ -14,28 +14,30 @@ public class buildRobot : MonoBehaviour
     void Start()
     {
         string jsonString = File.ReadAllText("Assets/Scripts/robot1.json");
-        GameObject robot1  = build(jsonString);
-        robot1.AddComponent<RoombaMovement>();
+        GameObject robot1 = build(jsonString);
         jsonString = File.ReadAllText("Assets/Scripts/robot2.json");
-        GameObject robot2  = build(jsonString);
-        robot2.AddComponent<RoombaMovement>();
+        GameObject robot2 = build(jsonString);
     }
 
-    void setParent(GameObject parent, GameObject child){
+    void setParent(GameObject parent, GameObject child)
+    {
         child.transform.parent = parent.transform;
     }
 
-    GameObject build(string jsonString){
+    GameObject build(string jsonString)
+    {
         var json = JSON.Parse(jsonString);
         int index = 0;
-        GameObject centerParent = null;
-        while (json[index] != null && centerParent == null)
+        GameObject parent = new GameObject("Parent");
+        parent.transform.position = new Vector3(0, 0.5f, 0);
+        parent.transform.rotation = Quaternion.Euler(90, 0, -90);
+        int centerX = 0, centerY = 0;
+        while (json[index] != null)
         {
             if (json[index]["type"] == "center")
             {
-                Vector3 pos = new Vector3((float)json[index]["x"], 0.5f, (float)json[index]["y"]);
-                Quaternion rot = Quaternion.Euler(90, 0, -90);
-                centerParent = Instantiate(center, pos, rot);
+                centerX = json[index]["x"];
+                centerY = json[index]["y"];
             }
             index++;
         }
@@ -43,48 +45,46 @@ public class buildRobot : MonoBehaviour
         while (json[index] != null)
         {
             string type = json[index]["type"];
-            if (type != "center")
+            GameObject childPart = null;
+            Vector3 pos = new Vector3((float)(json[index]["x"] - centerX), 0.5f, (float)(json[index]["y"] - centerY));
+            Quaternion rot;
+            string direction = json[index]["direction"];
+            switch (direction)
             {
-                GameObject childPart = null;
-                Vector3 pos = new Vector3((float)json[index]["x"], 0.5f, (float)json[index]["y"]);
-                Quaternion rot;
-                string direction = json[index]["direction"];
-                switch (direction)
-                {
-                    case "north":
-                        rot = Quaternion.Euler(90, 0, 0);
-                        break;
-                    case "south":
-                        rot = Quaternion.Euler(-90, 0, 0);
-                        break;
-                    case "east":
-                        rot = Quaternion.Euler(0, 0, -90);
-                        break;
-                    case "west":
-                        rot = Quaternion.Euler(0, 0, 90);
-                        break;
-                    default:
-                        rot = Quaternion.Euler(0, 0, 0);
-                        break;
-                }
-                switch (type)
-                {
-                    case "block":
-                        childPart = Instantiate(block, pos, rot);
-                        break;
-                    case "center":
-                        childPart = Instantiate(center, pos, rot);
-                        break;
-                    case "spike":
-                        childPart = Instantiate(spike, pos, rot);
-                        break;
-                }
-                setParent(centerParent,childPart);
+                case "north":
+                    rot = Quaternion.Euler(90, 0, 0);
+                    break;
+                case "south":
+                    rot = Quaternion.Euler(-90, 0, 0);
+                    break;
+                case "east":
+                    rot = Quaternion.Euler(0, 0, -90);
+                    break;
+                case "west":
+                    rot = Quaternion.Euler(0, 0, 90);
+                    break;
+                default:
+                    throw new NotImplementedException("Invalid JSON - direction");
             }
+            switch (type)
+            {
+                case "block":
+                    childPart = Instantiate(block, pos, rot);
+                    break;
+                case "center":
+                    childPart = Instantiate(center, pos, rot);
+                    break;
+                case "spike":
+                    childPart = Instantiate(spike, pos, rot);
+                    break;
+                default:
+                    throw new NotImplementedException("Invalid JSON - type");
+            }
+            setParent(parent, childPart);
             index++;
-
         }
-        return centerParent;
+        parent.AddComponent<RoombaMovement>();
+        return parent;
     }
 
     // Update is called once per frame
