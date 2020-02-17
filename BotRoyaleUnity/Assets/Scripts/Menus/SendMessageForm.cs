bbde9bc40f10c22c07e47e95bec5d2584ee017e4
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-16 18:04:18 -0800

    Add message form and message builder to unity

---
+++ BotRoyaleUnity/Assets/Scripts/Menus/SendMessageForm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessageForm : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_InputField InputElement = default;
+
+    private SocketConnectionHandler socketIO;
+
+    void Start()
+    {
+        socketIO = FindObjectOfType<SocketConnectionHandler>();
+    }
+
+    public void OnSubmitButtonClicked()
+    {
+        JSONObject dataObject = new JSONObject(InputElement.text);
+        socketIO.EmitGameMessage(dataObject);
+    }
+}
