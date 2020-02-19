6ca5e6f28dab807daf1dfb1644ccba0186af6f66
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-19 11:42:49 -0800

    Improve react app
    Split UI into components and made the code overall better

--- node-webservice/react-client/src/App.js
+++ node-webservice/react-client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import ConnectionMessage from './Components/ConnectionMessage';
+import JoinGameForm from './Components/JoinGameForm';
+import GameplayPage from './Components/GameplayPage';
+import ErrorPage from './Components/ErrorPage';
+
 import background from './background.jpg';
-import TextInput from './Components/TextInput';
-import RobotJSONObjectForm from './Components/RobotJSONObjectForm';
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link,
-  useParams
+  Route
 } from "react-router-dom";
 import socket from './API/socketHandler.js';
 
@@ -23,83 +24,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      connectedToAPI: false,
-      currentGameID: null
+      connectedToAPI: false
     };
-  }
 
-  callAPI() {
-      fetch("/api/test")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
+    this.renderAppBodyContent = this.renderAppBodyContent.bind(this);
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.callAPI();
-    }, 2000);
-
     socket.on('connect', () => {
       this.setState({ connectedToAPI: true });
     });
-  }
 
-  onGameIDSubmit(gameID) {
-    socket.emit('joingame', gameID, (returnedID) => {
-      this.setState({currentGameID: returnedID});
+    socket.on('disconnect', () => {
+      this.setState({ connectedToAPI: false });
     });
   }
 
-  onRobotDataSubmit(robotObj) {
-    socket.emit('game-message', { action: "submitrobot", parts: robotObj }, (err) => {
-      if (err == null) {
-        alert("sent robot data");
-      }else {
-        alert("error sending robot: " + err);
-      }
-    });
+  renderAppBodyContent() {
+    // show connecting message before the socket api responds
+    if (this.state.connectedToAPI !== true) {
+      return <ConnectionMessage/>;
+    }
+
+    // show a different component based on the URL path
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={ JoinGameForm } />
+          <Route path="/game/:gameid" exact component={ GameplayPage } />
+          <Route component={ ErrorPage } />
+        </Switch>
+      </Router>
+    );
   }
 
   render() {
-    if (this.state.connectedToAPI) {
-      if (this.state.currentGameID == null) {
-        return (
-          <Router>
-            <div className="App">
-              <header className="App-header">
-                <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
-                <h1 style={{textAlign:'center'}}>Build-a-Bot Royale</h1>
-                <img src='./logo.svg' style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
-              </header>
-              <div className="App-body" style={backgroundStyle}>
-                <Switch>
-                  <Route path="/" exact>
-                    <TextInput onSubmit={this.onGameIDSubmit.bind(this)}/>
-                  </Route>
-                  <Route path="/game">
-                    <p>Play game here!</p>
-                  </Route>
-                  <Route>
-                    <p>404! No route matched!</p>
-                  </Route>
-                </Switch>
-              </div>
-            </div>
-          </Router>
-        );
-      }else {
-        return (
-          <div>
-            <h1>Playing the game with id {this.state.currentGameID}!</h1>
-            <RobotJSONObjectForm onSubmit={ this.onRobotDataSubmit.bind(this) }/>
-          </div>
-        );
-      }
-    }else {
-      return (
-        <h1>Connecting...</h1>
-      )
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src='/logo.svg' alt="app logo" style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
+          <h1 style={{textAlign:'center'}}>Build-a-Bot Royale</h1>
+          <img src='/logo.svg' alt="app logo" style={{margin:'0.5vmin 3vmin', width:'calc(30px + 5vmin)'}}/>
+        </header>
+        <div className="App-body" style={backgroundStyle}>
+          { this.renderAppBodyContent() }
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ node-webservice/react-client/src/Components/ConnectionMessage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ConnectionMessage extends Component {
+  render() {
+    return <h1 className="connection-message">Connecting...</h1>;
+  }
+}
+
+export default ConnectionMessage;
