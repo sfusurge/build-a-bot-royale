db2750492f63a82bcff23423ccd472068cfd56a3
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-21 18:03:07 -0800

    Fix react warning

--- node-webservice/react-client/src/Components/HostGamePage.jsx
+++ node-webservice/react-client/src/Components/HostGamePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HostGamePage = props => (
     <div className="host-game-page">
         <h1>Host a game</h1>
-        <iframe frameborder="0" src="https://itch.io/embed-upload/1978326?color=333333" allowfullscreen="" width="640" height="380"><a href="https://buildabot.itch.io/build-a-bot-royale">Play Build-A-Bot Royale on itch.io</a></iframe>
+        <iframe title="game-embed" frameborder="0" src="https://itch.io/embed-upload/1978326?color=333333" allowfullscreen="" width="640" height="380"><a href="https://buildabot.itch.io/build-a-bot-royale">Play Build-A-Bot Royale on itch.io</a></iframe>
         <br/>
         <Link to="/">Back</Link>
     </div>
