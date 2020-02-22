bf9fa2eb7258d0254854f75e668c6bf3e323a4b1
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-21 17:23:11 -0800

    Add Itch.io embedded game to host page

--- node-webservice/react-client/src/Components/HostGamePage.jsx
+++ node-webservice/react-client/src/Components/HostGamePage.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const HostGamePage = props => (
     <div className="host-game-page">
         <h1>Host a game</h1>
-        <p>Download links to builds of the Unity app go here</p>
+        <iframe frameborder="0" src="https://itch.io/embed-upload/1978311?color=333333" allowfullscreen="" width="640" height="380"><a href="https://buildabot.itch.io/build-a-bot-royale">Play Build-A-Bot Royale on itch.io</a></iframe>
+        <br/>
         <Link to="/">Back</Link>
     </div>
 );
