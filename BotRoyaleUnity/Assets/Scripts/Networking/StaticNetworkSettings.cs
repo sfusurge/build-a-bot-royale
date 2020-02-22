198e2bf4d2195fc29dad2e0111af3a5638f0b6c1
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-21 18:34:51 -0800

    Move connection config into its own file

---
+++ BotRoyaleUnity/Assets/Scripts/Networking/StaticNetworkSettings.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public static class StaticNetworkSettings
+{
+    public static Servers CurrentServer = Servers.PRODUCTION;
+
+    static StaticNetworkSettings()
+    {
+        // by default, use local server in the editor, and deployed server in builds
+        if (Application.isEditor)
+        {
+            CurrentServer = Servers.LOCAL;
+        }
+        else
+        {
+            CurrentServer = Servers.PRODUCTION;
+        }
+    }
+
+    public static string ServerURL {
+        get
+        {
+            switch (CurrentServer)
+            {
+                case Servers.LOCAL:
+                    return "localhost";
+                case Servers.PRODUCTION:
+                    return "build-a-bot-royale.herokuapp.com";
+                default:
+                    throw new System.NotImplementedException("No server URL defined for server " + CurrentServer);
+            }
+        }
+    }
+
+    public static int ServerPort
+    {
+        get
+        {
+            switch (CurrentServer)
+            {
+                case Servers.LOCAL:
+                    return 9000;
+                case Servers.PRODUCTION:
+                    return 80;
+                default:
+                    throw new System.NotImplementedException("No server port defined for server " + CurrentServer);
+            }
+        }
+    }
+}
