96fd651a1ca66e1ff5a0266e8208d063707d6764
Jason Spence <jason.spence123@outlook.com>
2020-03-07 22:39:34 -0800

    Added inline CSS to make a left-justified list.

--- node-webservice/react-client/src/Components/AboutPage.jsx
+++ node-webservice/react-client/src/Components/AboutPage.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const myHeader = {
+    fontSize: '16pt',
+}
+
+const listContainer = {
+    display: 'flex',
+    alignItems: 'flex_eslkgndsnd',
+    width: 'auto',
+    //padding: '2em',
+    //background: '#eee',
+    margin: '0 auto',
+    textAlign: 'right',
+  }
+
+const ul = {
+    display: 'inline_block',
+    textAlign: 'left',
+  }
+
+
 const AboutPage = props => (
     <div className="about-page">
         <h1>About</h1>
-        <p>Info about the project goes here</p>
+        <p>Surge SFU Project Spring 2020</p> 
+        <div style={myHeader}>Created by:</div> 
+        <Contributors />
         <Link to="/">Back</Link>
     </div>
 );
+
+
+const Contributors = props => (
+    <div style={listContainer}>
+        <ul style={ul}>
+            <li>Thomas</li>
+            <li>Shea</li>
+            <li>Rustem</li>
+            <li>Katalina</li>
+            <li>Jason</li>
+            <li>Harry</li>
+        </ul>
+    </div>
+)
 
 export default AboutPage;
