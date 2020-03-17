020cd96a300bc5c9a96649c3856dfed53b79d8a1
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-03-16 22:49:39 -0700

    Move socket connection test code to TestsMain.js

--- node-webservice/test/SocketAPITests.js
+++ node-webservice/test/SocketAPITests.js
@@ -3,53 +3,13 @@ var expect = chai.expect;
 
 var io = require('socket.io-client');
 
-const PORT = require('./TestsMain').TEST_PORT;
-
-const initSocket = () => {
-    return new Promise((resolve, reject) => {
-      // create socket for communication
-      const socket = io('http://localhost:' + PORT, {
-        "reconnection delay": 0,
-        "reopen delay": 0,
-        "force new connection": true
-      });
-  
-      // define event handler for sucessfull connection
-      socket.on("connect", () => {
-        resolve(socket);
-      });
-    });
-  };
-  
-  const destroySocket = socket => {
-    return new Promise((resolve, reject) => {
-      // check if socket connected
-      if (socket.connected) {
-        // disconnect socket
-        socket.disconnect();
-        resolve(true);
-      } else {
-        // not connected
-        resolve(false);
-      }
-    });
-  };
-
+const TestUtils = require('./TestsMain');
+const CreateSocketClient = TestUtils.CreateSocketClient;
+const DestroySocketClient = TestUtils.DestroySocketClient;
 
 describe("Socket.io API", () => {
-/*
-    var socketClient;
-    beforeEach(done => {
-        // use a new socket client for each test
-        socketClient = io('http://localhost:' + PORT);
-
-        socketClient.on('connect', data => {
-            done();
-        });
-    });*/
-
     it ("receives the beep-boop test message", async () => {
-        const socketClient = await initSocket();
+        const socketClient = await CreateSocketClient();
         const sendMessage = new Promise((resolve, reject) => {
             socketClient.emit("beep", resolve);
             setTimeout(() => {
@@ -59,12 +19,7 @@ describe("Socket.io API", () => {
         const response = await sendMessage;
         expect(response.message).to.equal("boop");
 
-        await destroySocket(socketClient);
-        /*
-        socketClient.emit("beep", response => {
-            expect(response.message).to.equal("boop");
-        });*/
-        //expect(socketClient).to.not.be.null;
+        await DestroySocketClient(socketClient);
     });
 /*
     it ("responds to newgame with a game ID", done => {
--- node-webservice/test/TestsMain.js
+++ node-webservice/test/TestsMain.js
@@ -1,6 +1,8 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var io = require('socket.io-client');
+
 var server = require('../bin/testServer.js');
 const TEST_PORT = 9090;
 
@@ -17,4 +19,34 @@ afterEach(done => {
     });
 });
 
-module.exports = { TEST_PORT }
+const CreateSocketClient = () => {
+    return new Promise((resolve, reject) => {
+        // create socket connection
+        const socket = io('http://localhost:' + TEST_PORT, {
+            "reconnection delay": 0,
+            "reopen delay": 0,
+            "force new connection": true
+        });
+
+        // wait for socket to connect
+        socket.on("connect", () => {
+            resolve(socket);
+        });
+    });
+};
+  
+  const DestroySocketClient = socket => {
+    return new Promise((resolve, reject) => {
+        // check if socket connected
+        if (socket.connected) {
+            // disconnect socket
+            socket.disconnect();
+            resolve(true);
+        } else {
+            // not connected
+            resolve(false);
+        }
+    });
+  };
+
+module.exports = { TEST_PORT, CreateSocketClient, DestroySocketClient }
