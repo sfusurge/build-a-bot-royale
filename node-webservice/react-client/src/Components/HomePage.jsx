c1dfcaa8fee70ec91a64f4a5df03833ba1bce7d4
Jason Spence <jason.spence123@outlook.com>
2020-03-31 16:30:49 -0700

    Removed unnecessary CSS and div from around Join-Game form.

--- node-webservice/react-client/src/Components/HomePage.jsx
+++ node-webservice/react-client/src/Components/HomePage.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 import JoinGameForm from './JoinGameForm';
 import { Link } from 'react-router-dom';
 
-const listContainer = {
-    display: 'flex',
-    alignItems: 'flex_eslkgndsnd',
-    width: 'auto',
-    margin: '0 auto',
-}
-
 const HomePage = props => (
     <div className="home-page">
         <h1>Join a game</h1>
-        <div style={listContainer}>
-            <JoinGameForm {...props} style={listContainer}/>
-        </div>
+        <JoinGameForm {...props}/>
         <br/>
         <Link to="/host">Host a game</Link><br/>
         <Link to="/about">About</Link>
