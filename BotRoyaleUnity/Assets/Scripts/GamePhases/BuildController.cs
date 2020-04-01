c817a5e9c0f34e4359322a87abc92f394154a953
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-04-01 13:50:42 -0700

    Add game phase controller scripts and prefabs

---
+++ BotRoyaleUnity/Assets/Scripts/GamePhases/BuildController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+
+public class BuildController : GamePhaseController
+{
+    SocketConnectionHandler socketIO;
+
+    List<JSONObject> submitedRobots;
+
+    void Start()
+    {
+        socketIO = FindObjectOfType<SocketConnectionHandler>();
+        if (socketIO == null)
+        {
+            throw new MissingComponentException("BuildController needs socket");
+        }
+
+        submitedRobots = new List<JSONObject>();
+        socketIO.OnGameMessage("submitrobot", jsonObject =>
+        {
+            submitedRobots.Add(jsonObject);
+        });
+    }
+
+    public override JSONObject ReturnDataForNextGamePhase()
+    {
+        // put all submitted robots into a json object for the build phase to use
+        JSONObject submitedRobotsJSON = new JSONObject();
+        for (int i = 0; i < submitedRobots.Count; i++)
+        {
+            submitedRobotsJSON[i] = submitedRobots[i];
+        }
+        return submitedRobotsJSON;
+    }
+
+    public override void UseCarryOverData(JSONObject InputData)
+    {
+        // Do nothing. Nothing in this phase uses any carry over data from previous phase
+    }
+}
---
+++ BotRoyaleUnity/Assets/Scripts/GamePhases/ResultsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+
+public class ResultsController : GamePhaseController
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void UseCarryOverData(JSONObject InputData)
+    {
+        // TODO: use the carryover data to display the results in the ui
+    }
+
+    public override JSONObject ReturnDataForNextGamePhase()
+    {
+        return null;
+    }
+}
