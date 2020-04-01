17eeae72fe1b50f0841d71b6e27fc8f1109855af
Jason Spence <jason.spence123@outlook.com>
2020-03-31 20:10:02 -0700

    Converted 'Host a Game' link into a button.

--- node-webservice/react-client/src/Components/HomePage.jsx
+++ node-webservice/react-client/src/Components/HomePage.jsx
@@ -7,7 +7,13 @@ const HomePage = props => (
         <h1>Join a game</h1>
         <JoinGameForm {...props}/>
         <br/>
-        <Link to="/host">Host a game</Link><br/>
+        <form>
+            <button type="submit" formaction="/host" 
+            style={{width:'calc(60px + 14vmin)', fontSize: 'calc(10px + 2vmin)', textAlign:'center', color:'green'}}>
+                Host a Game
+            </button>
+        </form>
+        <br/>
         <Link to="/about">About</Link>
     </div>
 );
