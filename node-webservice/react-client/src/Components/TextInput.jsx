cace4b44a68440546f174bc94be36af42714c86a
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-04-29 13:38:34 -0700

    Add nickname field to join game page

--- node-webservice/react-client/src/Components/TextInput.jsx
+++ node-webservice/react-client/src/Components/TextInput.jsx
@@ -19,19 +19,14 @@ const submitStyle = {
 class TextInput extends Component {
     constructor(props) {
         super(props);
-        this.state = {value:''}
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { nickname: "", gameID: "" }
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event){
-        this.setState({value: event.target.value.toUpperCase()})
     }
 
     handleSubmit(event){
         this.props.onSubmit( {
-            gameID: this.state.value,
-            username: "default_username" // TODO: add a text field in this form for username
+            gameID: this.state.gameID,
+            username: this.state.nickname
         });
         
         event.preventDefault();
@@ -40,7 +35,20 @@ class TextInput extends Component {
     render() { 
         return ( 
             <form onSubmit={this.handleSubmit} style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-                <input type="text" placeholder="Game Code" style={inputStyle}value={this.state.value} onChange={this.handleChange}/>
+                <input
+                    type="text"
+                    placeholder="Nickname"
+                    style={ inputStyle }
+                    value={ this.state.nickname }
+                    onChange={ e => this.setState({ nickname: e.target.value })}
+                />            
+                <input
+                    type="text"
+                    placeholder="Game Code"
+                    style={ inputStyle }
+                    value={ this.state.gameID }
+                    onChange={ e => this.setState({ gameID: e.target.value })}
+                />
                 <input type="submit" value='Enter' style={submitStyle}/>
             </form>
          );
